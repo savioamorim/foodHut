b1321f3a7480a261996bf9e28cd1e4baa5c509d7
Savio Amorim <savioamorim.s@hotmail.com>
2021-10-27 21:37:38 -0300

    First Commit

---
+++ src/main/java/com/cotemig/foodHut/FoodHutApplication.java
@@ -0,0 +1,13 @@
+package com.cotemig.foodHut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodHutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodHutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cotemig/foodHut/FoodHutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotemig.foodHut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodHutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
